c8607cf7df3a1421de744c78bdfac81022f39eed
Nanored4498 <nanored.mc@gmail.com>
2026-02-10 17:33:54 +0100

    removing folder and using a cache of futures

--- joblib/shelf.py
+++ joblib/shelf.py
@@ -1,32 +1,73 @@
-from os import getpid
+import os
+import shutil
 from uuid import uuid4
 
 from ._memmapping_reducer import _get_temp_dir
-from .memory import StoreBase
+from .memory import _store_backend_factory
 
 _shelf = None
+_futures = dict()
 
 
 class ShelfFuture(object):
-    def __init__(self, store_backend, id):
-        self.store_backend = store_backend
+    def __init__(self, location, id):
+        self.location = location
         self.id = id
 
     def result(self):
-        return self.store_backend.load_item((self.id,))
+        global _futures
+        id = (self.location, self.id)
+        value = _futures.get(id, None)
+        if value is None:
+            value = _store_backend_factory("local", self.location).load_item((self.id,))
+            _futures[id] = value
+        return value
 
 
-class Shelf(StoreBase):
-    _folder_name = f"joblib_shelf_{getpid()}"
+class Shelf(object):
+    """An object for storing values to be used later"""
+
+    def __init__(self, mmap_mode=None, backend_options=None):
+        self.mmap_mode = mmap_mode
+        if backend_options is None:
+            backend_options = {}
+
+        location = _get_temp_dir("")[0]
+        location = os.path.join(location, f"joblib_shelf_{os.getpid()}")
+
+        self.store_backend = _store_backend_factory(
+            "local",
+            location,
+            verbose=1,
+            backend_options=dict(
+                compress=False, mmap_mode=mmap_mode, **backend_options
+            ),
+        )
 
     def shelve(self, data):
         id = uuid4().hex
         self.store_backend.dump_item((id,), data)
-        return ShelfFuture(self.store_backend, id)
+        return ShelfFuture(self.store_backend.location, id)
+
+    def clear(self):
+        """Erase the complete storage directory."""
+        if self.store_backend is not None:
+            self.store_backend.clear()
+
+    def __repr__(self):
+        return "{class_name}(location={location})".format(
+            class_name=self.__class__.__name__,
+            location=(
+                None if self.store_backend is None else self.store_backend.location
+            ),
+        )
+
+    def __del__(self):
+        shutil.rmtree(self.store_backend.location)
 
 
 def shelve(data):
     global _shelf
     if _shelf is None:
-        _shelf = Shelf(_get_temp_dir("")[0])
+        _shelf = Shelf()
     return _shelf.shelve(data)
